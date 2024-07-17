f8490584b332b7158df489d2d56eb4186977cba1
R <rqou@berkeley.edu>
2024-07-17 23:34:06 +0100

    Implement single_threaded pthread_exit
    Performs all the cancellation actions and then calls exit()

--- libc-top-half/stub-pthreads/stub-pthreads.c
+++ libc-top-half/stub-pthreads/stub-pthreads.c
@@ -5,6 +5,7 @@ static void dummy_0()
 }
 weak_alias(dummy_0, __acquire_ptc);
 weak_alias(dummy_0, __release_ptc);
+weak_alias(dummy_0, __pthread_tsd_run_dtors);
 
 int __pthread_create(pthread_t *restrict res, const pthread_attr_t *restrict attrp, void *(*entry)(void *), void *restrict arg)
 {
@@ -50,4 +51,28 @@ int pthread_once(pthread_once_t *control, void (*init)(void))
 		*control = 1;
 	}
 	return 0;
+}
+
+_Noreturn void pthread_exit(void *result)
+{
+	/*
+		We are the only thread, so when we exit the whole process exits.
+		But we still have to run cancellation handlers...
+	*/
+	pthread_t self = __pthread_self();
+
+	self->canceldisable = 1;
+	self->cancelasync = 0;
+	self->result = result;
+
+	while (self->cancelbuf) {
+		void (*f)(void *) = self->cancelbuf->__f;
+		void *x = self->cancelbuf->__x;
+		self->cancelbuf = self->cancelbuf->__next;
+		f(x);
+	}
+
+	__pthread_tsd_run_dtors();
+
+	exit(0);
 }
